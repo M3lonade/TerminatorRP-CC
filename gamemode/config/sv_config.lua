e61d40a53decbfef2f7a9ed161b8cd7ff3055ef1
M3lonade <47722033+M3lonade@users.noreply.github.com>
2023-12-15 00:01:51 +0300

    Add files via upload

---
+++ gamemode/config/sv_config.lua
@@ -0,0 +1,31 @@
+GM.MySQLType 	= "mysqloo"
+
+GM.MySQLHost 	= "gspdb-chi.hosthavoc.com"
+GM.MySQLPort 	= 3306
+GM.MySQLDB 		= "db164989"
+
+GM.MySQLUser 	= "db164989"
+GM.MySQLPass 	= "7HJHC8uDFO"
+
+GM.MySQLAliases = {
+	chars 			= "cc_chars",
+	players 		= "cc_players",
+	bans 			= "cc_bans",
+	donations 		= "cc_donations",
+	notes 			= "cc_notes",
+	items 			= "cc_items",
+	logs 			= "cc_logs",
+	logcharacters 	= "cc_logs_characters",
+	logitems 		= "cc_logs_items",
+	logplayers 		= "cc_logs_players"
+}
+
+GM.EnablePrometheus 	= false
+GM.PrometheusServerID 	= 3
+
+GM.PrometheusSQLHost 	= "localhost"
+GM.PrometheusSQLPort 	= 3306
+GM.PrometheusSQLDB 		= "donate"
+
+GM.PrometheusSQLUser 	= "root"
+GM.PrometheusSQLPass 	= ""
